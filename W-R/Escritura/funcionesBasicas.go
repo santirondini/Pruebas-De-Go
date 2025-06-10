0c2191c78df398e91500d46d777f10f41bc3a5aa
santirondini <srondini@frba.utn.edu.ar>
2025-06-10 03:46:22 -0300

    Create funcionesBasicas.go

---
+++ W-R/Escritura/funcionesBasicas.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+
+func main(){
+
+
+	memoriaPrincipal := make([]byte, 4096)
+
+	holaMundo := []byte("Hola, mundo!")
+
+	memoriaPrincipal = append(memoriaPrincipal, holaMundo...)
+
+	fmt.Println("Contenido del slice de bytes: ")
+	for i := 0; i < 10; i++ {
+		fmt.Println("memoriaPrincipal[i]: ", memoriaPrincipal[i])
+	}
+
+	// Cada letra del string vale 1 byte, por lo que el tamaño de cada letra es 1 byte
+	var hola = "Hola" // 4
+	var coma = "," // 1
+	var mundo = "mundo" // 5
+	var exclamacion = "!" // 1
+	var letra = 'a' // 1
+	fmt.Println("copy(memoriaPrincipal[0:4], hola)") 
+	copy(memoriaPrincipal[0:4], hola) // Copia "Hola" en memoriaPrincipal
+
+	// [72 111 108 97 0 0 0 .. ] => Son lo valores ASCII de cada letra
+ 
+	fmt.Println("Tamaño de: ")
+	fmt.Println("Tamaño de memoriaPrincipal: ", len(memoriaPrincipal))
+	fmt.Println("Tamaño en bytes de hola: ", len(hola))
+	fmt.Println("Tamaño en bytes de coma: ", len(coma))
+	fmt.Println("Tamaño en bytes de mundo: ", len(mundo))
+	fmt.Println("Tamaño en bytes de exclamacion: ", len(exclamacion))
+	fmt.Println("Tamaño en bytes de letra: ", len(string(letra))) 
+
+	fmt.Println("")
+
+
+	fmt.Println("Tamaño del slice de bytes: ", len(memoriaPrincipal))
+	fmt.Println("Capacidad del slice de bytes: ", cap(memoriaPrincipal))	
+	fmt.Println("&memoriaPrincipal[0]: ", &memoriaPrincipal[0]) // Imprime la dirección de memoria del primer elemento del slice
+	// fmt.Println("Dirección de memoria del slice: ", &memoriaPrincipal) // Imprime la dirección de memoria del slice completo
+	// fmt.Println("memoriaPrincipal: ", memoriaPrincipal)
+
+	fmt.Println("memoriaPrincipal[0:4]: ", memoriaPrincipal[0:4]) // Imprime los primeros 4 bytes del slice
+	fmt.Println("memoriaPrincipal[0:4] como string: ", string(memoriaPrincipal[0:4])) // Imprime los primeros 4 bytes del slice como string
+}
